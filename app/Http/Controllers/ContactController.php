656f43adaa98db94514cc6d9e60417f33691a068
Clivet <clivetlungu0@gmail.com>
2025-10-06 15:41:21 +0200

    emails

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -33,7 +33,7 @@ class ContactController extends Controller
             $data['inquiry_type_label'] = $this->getInquiryTypeLabel($data['inquiry_type']);
 
             // Send email to Violet
-            Mail::to(config('mail.contact_email', 'violet@violetnswanakaponda.com.com'))
+            Mail::to(config('mail.contact_email', 'violet@violetnswanakaponda.com'))
                 ->send(new ContactInquiry($data));
 
             // Send confirmation to inquirer
